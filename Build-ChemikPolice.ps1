eb159bf9c5e2564d2b925cdc5a65111d9f713cb6
michalantolik <michal.antolik@gmail.com>
2023-06-28 08:05:02 +0200

    Build and push chemik-police to DockerHub

---
+++ Build-ChemikPolice.ps1
@@ -0,0 +1,2 @@
+docker image build -f ChemikPolice/ChemikPoliceApp/Dockerfile -t michalantolik/chemik-police:latest .
+docker image build -f ChemikPolice/ChemikPoliceApp/Dockerfile -t michalantolik/chemik-police:1.0 .
---
+++ Push-ChemikPolice.ps1
@@ -0,0 +1,2 @@
+docker image push michalantolik/chemik-police:latest
+docker image push michalantolik/chemik-police:1.0
