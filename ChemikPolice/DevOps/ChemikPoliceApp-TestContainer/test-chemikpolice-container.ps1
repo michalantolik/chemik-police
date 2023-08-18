37e1ca0ef50a81b7bd55ea78907f99b4cd48b08d
michalantolik <michal.antolik@gmail.com>
2023-08-18 13:48:56 +0200

    Write more output in container testing script

--- ChemikPolice/DevOps/ChemikPoliceApp-TestContainer/test-chemikpolice-container.ps1
+++ ChemikPolice/DevOps/ChemikPoliceApp-TestContainer/test-chemikpolice-container.ps1
@@ -1,12 +1,14 @@
 $count = 0
 do {
     $count++
-    Write-Output "[$env:STAGE_NAME] Testing chemikpolice container [Attempt: $count]"
-    
+    Write-Output "[$env:STAGE_NAME] Testing chemikpolice app running in container [Attempt: $count]"
+    Write-Output "[$env:STAGE_NAME] Running command: 'Invoke-WebRequest -Uri http://host.docker.internal:32769'"
+
     $testStart = Invoke-WebRequest -Uri http://host.docker.internal:32769
     
     if ($testStart.statuscode -eq '200') {
         $started = $true
+        Write-Output "[$env:STAGE_NAME] StatusCode == 200 --> chemcikpolice app is running fine"
     } else {
         Start-Sleep -Seconds 1
     }
@@ -14,5 +16,6 @@ do {
 } until ($started -or ($count -eq 3))
 
 if (!$started) {
+    Write-Output "[$env:STAGE_NAME] StatusCode != 200 --> chemcikpolice app failed to run !!!"
     exit 1
 }
