06c4d3e0189aab10bf0880906ffe65483051e5b1
michalantolik <michal.antolik@gmail.com>
2023-08-20 16:18:24 +0200

    Fix Anchore setup srcript - remove dir if exists

--- ChemikPolice/DevOps/Anchore-Setup-in-Jenkins/compose-anchore-engine.sh
+++ ChemikPolice/DevOps/Anchore-Setup-in-Jenkins/compose-anchore-engine.sh
@@ -1,7 +1,13 @@
 #! /bin/sh
 
-mkdir anchore
-cd anchore
+if [ -d "anchore"]
+then
+    echo "Removing anchore directory"
+    rm -rf "anchore"
+fi
+
+mkdir "anchore"
+cd "anchore"
 
 curl -O https://engine.anchore.io/docs/quickstart/docker-compose.yaml > docker-compose.yaml
 docker compose up -d
